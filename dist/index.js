7bd0b73399c85b76107bc92452e9260683622e86
RichardHpa <richard.hpa@hotmail.com>
2020-01-09 20:53:38 +1300

    set up project for image cropper

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RhCropper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement("div", null);
+  }
+
+}
